9a3701a7a1c0989b7e08b90eca8a63d8c438c477
lixxie29 <crina.ciocan@stud.ubbcluj.ro>
2024-04-30 01:46:16 +0300

    1:46am messages

--- admin_contacts.php
+++ admin_contacts.php
@@ -10,6 +10,12 @@ if(!isset($admin_id)){
     header('location:login.php');
 };
 
+if(isset($_GET['delete'])){
+    $delete_id = $_GET['delete'];
+    mysqli_query($conn, "DELETE FROM `message` WHERE id = '$delete_id'") or die('query failed');
+    header('location:admin_contacts.php');
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +37,36 @@ if(!isset($admin_id)){
 
 <?php @include 'admin_header.php'; ?>
 
+<section class="messages">
+
+    <h1 class="title">messages</h1>
+
+
+    <div class="box-container">
+
+        <?php
+        $select_message = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+        if(mysqli_num_rows($select_message) > 0)
+        {
+            while($fetch_message = mysqli_fetch_assoc($select_message)){
+        ?>
+        <div class="box">
+            <p>user id : <span><?php echo $fetch_message['user_id']; ?> </span> </p>
+            <p>name : <span><?php echo $fetch_message['name']; ?> </span> </p>
+            <p>number : <span><?php echo $fetch_message['number']; ?> </span> </p>
+            <p>email : <span><?php echo $fetch_message['email']; ?> </span> </p>
+            <p>message : <span><?php echo $fetch_message['message']; ?> </span> </p>
+            <a href="admin_contacts.php?delete=<?php echo $fetch_message['id'];?>" onClick="return confirm('delete this message?');" class="delete-btn">delete</a>
+        </div>
+        <?php
+            }
+        }else{
+            echo '<p class="empty">no current messages</p>';
+        }
+        ?>
+    </div>
+
+</section>
 
 
 
