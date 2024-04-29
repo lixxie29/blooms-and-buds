c531f22a6fb4e9323c6317f8af050f47956014e3
lixxie29 <crina.ciocan@stud.ubbcluj.ro>
2024-04-29 20:17:47 +0300

    8:17pm

--- admin_products.php
+++ admin_products.php
@@ -12,57 +12,31 @@ if(!isset($admin_id)){
 
 if(isset($_POST['add_product'])){
 
-    // passing input through the mysqli_real_escape_string() function, any special characters that could be used to launch an SQL injection attack are neutralized, making the query safer and more secure.
-
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $price = mysqli_real_escape_string($conn, $_POST['price']);
     $details = mysqli_real_escape_string($conn, $_POST['details']);
-    $image = $_FILES['image'];
-    // $image_size = $_FILES['image']['size'];
-    // $image_tmp_name = $_FILES['image']['tmp_name'];
-    // $image_folder = "/project/Flower-Store-Website/uploaded_img".$image;
+    $image = $_FILES['image']['name'];
+    $image_size = $_FILES['image']['size'];
+    $image_tmp_name = $_FILES['image']['tmp_name'];
+    $image_folter = 'uploaded_img/'.$image;
+ 
+    $select_product_name = mysqli_query($conn, "SELECT name FROM `products` WHERE name = '$name'") or die('query failed');
+ 
+    if(mysqli_num_rows($select_product_name) > 0){
+       $message[] = 'product name already exist!';
+    }else{
+       $insert_product = mysqli_query($conn, "INSERT INTO `products`(name, details, price, image) VALUES('$name', '$details', '$price', '$image')") or die('query failed');
+ 
+       if($insert_product){
+          if($image_size > 2000000){
+             $message[] = 'image size is too large!';
+          }else{
+             move_uploaded_file($image_tmp_name, $image_folter);
+             $message[] = 'product added successfully!';
+          }
+       }
+    }   
 
-    $select_product_name_query = "SELECT name FROM `products` WHERE name = '$name'";
-    $select_product_name = mysqli_query($conn, $select_product_name_query) or die('query failed');
-
-
-    if(mysqli_num_rows($select_product_name) > 0)
-    {
-        $message[] = 'product name already exists';
-    }
-    else {
-        $insert_product_query = "INSERT INTO `products` (name, details, price, image) VALUES ('$name', '$details', '$price', '$image')";
-        $insert_product = mysqli_query($conn, $insert_product_query) or die('query failed');
-
-            if($insert_product) {
-            // if($image_size > 2000000) {
-            //     $message[] = 'image size is too large';
-            // } else{
-            //     move_uploaded_file($_FILES["image"]["tmp_name"], $image_folder);
-            //     $message[] = 'product added successfully';
-            // }
-
-            $time = time(); //used to make each image
-            $image_name = $time . $image['name'];
-            $image_tmp_name = $image['tmp_name'];
-            $image_destination_path = '/project/Flower-Store-Website/images' . $image_name;
-
-            //make sure file is image
-
-            $allowed_files = ['png', 'jpg', 'jpeg'];
-            $image_extension = explode('.', $image_name);
-            $image_extension = end($image_extension);
-            if (in_array($image_extension, $allowed_files)) {
-                //proceed to check size
-                if ($image['size'] < 2000000) {
-                    //upload the image
-                    move_uploaded_file($image_tmp_name, $image_destination_path);
-                    $message[] = 'product added successfully';
-                }
-            }
-            else {$message[] = 'image size is too large';}
-        }
-    }    
  }
 
  if(isset($_GET['delete'])){
