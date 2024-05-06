cbcaae30280ad411a64f14ff2fd5a03904971d79
lixxie29 <crina.ciocan@stud.ubbcluj.ro>
2024-05-06 18:12:32 +0300

    6:12pm

--- home.php
+++ home.php
@@ -44,6 +44,45 @@ if(!isset($user_id)){
 </section>
 
 
+<section class="products">
+    <h1 class="title">latest products</h1>
+
+    <div class="box-container">
+
+    <?php
+        $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+        if(mysqli_num_rows($select_products) > 0){
+            while($fetch_products = mysqli_fetch_assoc($select_products)){
+        ?>
+        <form action="" method="POST" class="box">
+            <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
+            <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+            <img src="images/<?php echo $fetch_products['image']; ?>" alt="" class="image">
+            <div class="name"><?php echo $fetch_products['name']; ?></div>
+            <input type="number" name="product_quantity" value="1" min="0" class="qty">
+            <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+            <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+            <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+            <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+            <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+            <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+
+        </form>
+
+        <?php
+            }
+        }else{
+             echo '<p class="empty">no products added yet</p>';
+        }
+    ?>
+
+    </div>
+
+    <div class="more-btn">
+        <a href="shop.php" class="option-btn">load more products</a>
+    </div>
+</section>
+
 
 
 <?php @include 'footer.php';?>
