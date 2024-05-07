efa84ae0cdd037cbdfb759cf8af866498eb2d449
lixxie29 <crina.ciocan@stud.ubbcluj.ro>
2024-05-07 17:50:05 +0300

    5:49pm

--- about.php
+++ about.php
@@ -31,6 +31,129 @@ if(!isset($user_id)){
 <?php @include 'header.php';?>
 
 
+<section class="section heading">
+    <h3>about us</h3>
+    <p> <a href="home.php">home</a> / about</p>
+</section>
+
+<section class="about">
+    <div class="flex">
+        <div class="image">
+            <img src="images/about-img1.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>At Blooms & Buds, we believe 
+                in the power of flowers to brighten your day, elevate your space, and create 
+                unforgettable moments. Our handcrafted bouquets are carefully curated by our 
+                team of expert florists, using only the freshest, highest quality blooms sourced 
+                from local growers and sustainable farms.</p>
+                <a href="shop.php" class="btn">shop our products</a>
+        </div>
+    </div>
+
+    <div class="flex">
+        <div class="content">
+            <h3>what do we offer?</h3>
+            <p>At Blooms & Buds, we believe 
+                in the power of flowers to brighten your day, elevate your space, and create 
+                unforgettable moments. Our handcrafted bouquets are carefully curated by our 
+                team of expert florists, using only the freshest, highest quality blooms sourced 
+                from local growers and sustainable farms.</p>
+                <a href="contact.php" class="btn">contact us</a>
+        </div>
+
+        <div class="image">
+            <img src="images/about-img2.jpg" alt="">
+        </div>
+</div>
+
+    <div class="flex">
+        <div class="image">
+            <img src="images/about-img3.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>who we are</h3>
+            <p>At Blooms & Buds, we believe 
+                in the power of flowers to brighten your day, elevate your space, and create 
+                unforgettable moments. Our handcrafted bouquets are carefully curated by our 
+                team of expert florists, using only the freshest, highest quality blooms sourced 
+                from local growers and sustainable farms.</p>
+                <a href="#reviews" class="btn">client reviews</a>
+        </div>
+    </div>
+</section>
+
+<section class="reviews" id="reviews">
+
+    <h1 class="title">client reviews</h1>
+
+    <div class="box-container">
+        <div class="box">
+            <img src="images/pic1.jpg" alt="">
+            <p>At Blooms & Buds, we believe 
+            in the power of flowers to brighten your day, elevate your space, and create 
+            unforgettable moments.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic2.jpg" alt="">
+            <p>At Blooms & Buds, we believe 
+            in the power of flowers to brighten your day, elevate your space, and create 
+            unforgettable moments.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic3.jpg" alt="">
+            <p>At Blooms & Buds, we believe 
+            in the power of flowers to brighten your day, elevate your space, and create 
+            unforgettable moments.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic4.jpg" alt="">
+            <p>At Blooms & Buds, we believe 
+            in the power of flowers to brighten your day, elevate your space, and create 
+            unforgettable moments.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>john deo</h3>
+        </div>
+
+    </div>
+
+</section>
 
 
 
--- home.php
+++ home.php
@@ -10,6 +10,52 @@ if(!isset($user_id)){
     header('location:login.php');
 }
 
+if(isset($_POST['add_to_wishlist'])){
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+
+    $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    if(mysqli_num_rows($check_wishlist_numbers) > 0){
+        $message[] = 'already added to wishlist';
+    }elseif(mysqli_num_rows($check_cart_numbers) > 0){
+        $message[] = 'already added to cart';
+    }else{
+        mysqli_query($conn, "INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES ('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die('query failed');
+        $message[] = 'product added to wishlist';
+    }
+}
+
+
+if(isset($_POST['add_to_cart'])){
+
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+    if(mysqli_num_rows($check_cart_numbers) > 0){
+        $message[] = 'already added to cart';
+    }else{
+
+        $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+        if(mysqli_num_rows($check_wishlist_numbers) > 0){
+            mysqli_query($conn, "DELETE FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+        }
+
+        mysqli_query($conn, "INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES ('$user_id', '$product_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+        $message[] = 'product added in cart';
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -36,9 +82,7 @@ if(!isset($user_id)){
         <p>Your premier destination for exquisite artisanal 
             bouquets and delightful monthly subscription boxes. At Blooms & Buds, we believe 
             in the power of flowers to brighten your day, elevate your space, and create 
-            unforgettable moments. Our handcrafted bouquets are carefully curated by our 
-            team of expert florists, using only the freshest, highest quality blooms sourced 
-            from local growers and sustainable farms.</p>
+            unforgettable moments. </p>
         <a href="about.php" class="btn">discover more</a>
     </div>
 </section>
@@ -66,7 +110,6 @@ if(!isset($user_id)){
             <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
             <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
             <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-
         </form>
 
         <?php
@@ -82,6 +125,18 @@ if(!isset($user_id)){
         <a href="shop.php" class="option-btn">load more products</a>
     </div>
 </section>
+
+
+<section class="home-contact">
+    <div class="content">
+        <h3>get in touch with us</h3>
+        <p>Our handcrafted bouquets are carefully curated by our 
+        team of expert florists, using only the freshest, highest quality blooms sourced 
+        from local growers and sustainable farms.</p>
+        <a href="contact.php" class="btn">contact us</a>
+    </div>
+</section>
+
 
 
 
