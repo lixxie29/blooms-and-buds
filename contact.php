406130c2f473ac1460f2f50815a2f5eced0d8a0a
lixxie29 <crina.ciocan@stud.ubbcluj.ro>
2024-05-08 18:33:57 +0300

    6:33pm

--- contact.php
+++ contact.php
@@ -10,6 +10,23 @@ if(!isset($user_id)){
     header('location:login.php');
 }
 
+if(isset($_POST['send'])){
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $number = mysqli_real_escape_string($conn, $_POST['number']);
+    $msg = mysqli_real_escape_string($conn, $_POST['message']);
+
+    $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name = '$name' AND email = '$email' and number = '$number' AND message = '$msg'") or die('query failed');
+
+    if(mysqli_num_rows($select_message) > 0){
+        $message[] = 'message sent already';
+    }else{
+        mysqli_query($conn, "INSERT INTO `message`(user_id, name, email, number, message) VALUES ('$user_id', '$name', '$email', '$number', '$msg')") or die('query failed');
+        $message[] = 'message sent successfully';
+    }
+}
+
+
 ?>
 
 <!DOCTYPE html>
